b17a661a933e3e8b1425bc25f34519f65014e862
Rupinderkk17 <109845928+Rupinderkk17@users.noreply.github.com>
2022-07-27 18:57:58 +0530

    Initial commit

---
+++ app/src/main/java/com/rupinder/relativelayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rupinder.relativelayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
